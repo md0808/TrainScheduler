40a1e1fc687e81b979c5d0ae6a1e5399bd2a40c2
Monica <monica.dixon8@gmail.com>
2019-10-20 21:38:28 -0500

    improved CSS, worked on remove button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -13,13 +13,35 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
+
+
+//click events =============================================================================
+
+$(document).on('click','.remove-btn', function (){
+
+  //This only works locally
+  $(this).parent().remove();
+
+  //The key that is generated from database.ref().push().getKey(); doesn't return the 
+  //same ID as what shows in the database
+
+  var databaseName = "train-scheduler-6bd9b";
+  //data key assigned at line 101
+  var trainID = $(this).parent().data('key');
+
+  var path = database.ref(databaseName).child(trainID);
+  path.remove();
+
+  }); 
+
+//Create an object out of variables and sends to firebase
 $('#add-train-btn').on('click', function(event){
     event.preventDefault();
 
-    var trainName= $('#train-name-input').val().trim();
-    var destination= $('#destination-input').val().trim();
-    var frequency= $('#frequency-input').val().trim();
-    var arrival= moment($('#arrival-input').val().trim(), "HH:mm").format("HH:mm");
+    var trainName= $("#train-name-input").val().trim();
+    var destination= $("#destination-input").val().trim();
+    var frequency= $("#frequency-input").val().trim();
+    var arrival= moment($("#arrival-input").val().trim(), "HH:mm").format("HH:mm");
     var weekendsAndHolidays= $("input:radio:checked").val();
 
     var newTrain = {
@@ -31,19 +53,25 @@ $('#add-train-btn').on('click', function(event){
       };
 
     database.ref().push(newTrain);
-    
+    var key = database.ref().push().getKey();
+    console.log(key);      
 
       $("#train-name-input").val("");
       $("#destination-input").val("");
       $("#frequency-input").val("");
       $("#arrival-input").val("");
-      $("#arrival-input").val("No");
+      $("#holiday-input").val("No");
 
 })
 
-database.ref().on("child_added", function(childSnapshot) {
 
+//Event listener that brings back a snapshot of what the database looks like when a Child is added ==============================================================
+
+database.ref().on("child_added", function(childSnapshot) {
+  
+  var trainRef = database.ref().push().getKey(); 
  
+
   var dataTrainName = childSnapshot.val().name;
   var dataDestination = childSnapshot.val().destination;
   var dataFrequency = childSnapshot.val().frequency;
@@ -51,24 +79,28 @@ database.ref().on("child_added", function(childSnapshot) {
   var dataWeekends = childSnapshot.val().weekendsAndHolidays;
 
 
-  var dataFirstArrivalConverted = moment(dataFirstArrival, "HH:mm", true).subtract(1, "years");
+  var dataFirstArrivalConverted = moment(dataFirstArrival, "HH:mm").subtract(1, "years");
   var diffTime = moment().diff(moment(dataFirstArrivalConverted), "minutes");
   var tRemainder = diffTime % dataFrequency;
   var tMinutesTillTrain = dataFrequency - tRemainder;
   var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-  var nextArrival = moment(nextTrain).format("hh:mm");
+  var nextArrival = moment(nextTrain).format("HH:mm");
 
 
-  var newTableRow = $('<tr>')
+  var newTableRow = $('<tr id= "row-' + trainRef + '">')
+  var removeButton = $('<button class="remove-btn">' + "Remove" + '</button>');
   var newTDName = $('<td>'+ dataTrainName + '</td>');
   var newTDDestination = $('<td>'+ dataDestination + '</td>');
-  var newTDFrequency = $('<td>'+ dataFrequency+ '</td>');
+  var newTDFrequency = $('<td>'+ dataFrequency+ " minutes" +'</td>');
   var newTDArrival = $('<td>'+ nextArrival + '</td>');
-  var newTDMinutesAway = $('<td>'+ tMinutesTillTrain + '</td>');
+  var newTDMinutesAway = $('<td>'+ tMinutesTillTrain + " minutes away" +'</td>');
   var newTDWeekends= $('<td>'+ dataWeekends + '</td>');
 
 
-  $('#train-table-body').append(newTableRow);
+  $("#train-table-body").append(newTableRow);
+  //assigns the key as a data attribute (the key) to the whole row
+  newTableRow.attr("data-key", trainRef);
+  newTableRow.append(removeButton);
   newTableRow.append(newTDName);
   newTableRow.append(newTDDestination);
   newTableRow.append(newTDFrequency);
@@ -78,18 +110,51 @@ database.ref().on("child_added", function(childSnapshot) {
 });
 
 
+// clears and reloads data 
+function updateTime(){
+  $("#train-table-body").empty();
 
+  database.ref().on("child_added", function(childSnapshot) {
+  
+    var trainRef = database.ref().push().getKey(); 
 
-// * Users from many different machines must be able to view same train times.
+    var dataTrainName = childSnapshot.val().name;
+    var dataDestination = childSnapshot.val().destination;
+    var dataFrequency = childSnapshot.val().frequency;
+    var dataFirstArrival = childSnapshot.val().arrival;
+    var dataWeekends = childSnapshot.val().weekendsAndHolidays;
+  
+  
+    var dataFirstArrivalConverted = moment(dataFirstArrival, "HH:mm").subtract(1, "years");
+    var diffTime = moment().diff(moment(dataFirstArrivalConverted), "minutes");
+    var tRemainder = diffTime % dataFrequency;
+    var tMinutesTillTrain = dataFrequency - tRemainder;
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var nextArrival = moment(nextTrain).format("HH:mm");
+  
+  
+    var newTableRow = $('<tr id= "row-' + trainRef + '">')
+    var removeButton = $('<button class="remove-btn">' + "Remove" + '</button>');
+    var newTDName = $('<td>'+ dataTrainName + '</td>');
+    var newTDDestination = $('<td>'+ dataDestination + '</td>');
+    var newTDFrequency = $('<td>'+ dataFrequency+ " minutes" +'</td>');
+    var newTDArrival = $('<td>'+ nextArrival + '</td>');
+    var newTDMinutesAway = $('<td>'+ tMinutesTillTrain + " minutes away" +'</td>');
+    var newTDWeekends= $('<td>'+ dataWeekends + '</td>');
+  
+  
+    $("#train-table-body").append(newTableRow);
+    newTableRow.attr("data-key", trainRef);
+    newTableRow.append(removeButton);
+    newTableRow.append(newTDName);
+    newTableRow.append(newTDDestination);
+    newTableRow.append(newTDFrequency);
+    newTableRow.append(newTDArrival);
+    newTableRow.append(newTDMinutesAway);
+    newTableRow.append(newTDWeekends);  
 
-// ### Bonus (Extra Challenges)
+  });  
 
-// * Consider updating your "minutes to arrival" and "next train time" text once every minute. This is significantly more challenging; only attempt this if you've completed the actual activity and committed it somewhere on GitHub for safekeeping (and maybe create a second GitHub repo).
-
-// * Try adding `update` and `remove` buttons for each train. Let the user edit the row's elements-- allow them to change a train's Name, Destination and Arrival Time (and then, by relation, minutes to arrival).
-
-// * As a final challenge, make it so that only users who log into the site with their Google or GitHub accounts can use your site. You'll need to read up on Firebase authentication for this bonus exercise.
-//==================================================================================================
-
-
+}
+setInterval(updateTime, 60000);
 
